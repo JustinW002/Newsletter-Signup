c6ab788a0fac80c381be2656082e62a080b1a4af
Justin Woods <justinwoods002@gmail.com>
2020-01-10 00:11:09 -0700

    add static public folder to app.js create get and post method for root. Modify form in signup.html to call post method

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.static("public"));
 app.listen(3000,function(){
     console.log("Connected to port 3000");
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    console.log(req.body);
 });
